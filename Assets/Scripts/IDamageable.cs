1747ebff7e0e0d4f416e593008606ac26868c502
DESKTOP-BD9PTPU\Park <soda3115@gmail.com>
2020-04-12 22:27:05 +0900

    - 3주차

---
+++ Assets/Scripts/IDamageable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+//피격 처리를 담당하는 인터페이스.
+//해당 인터페이스를 상속 받지 않는 경우 피격 처리가 되지 않는다.
+public interface IDamageable
+{
+    void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal);
+}
